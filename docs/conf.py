c2181497ea98ee4a760a127ff15582c5a6e6729e
Patrick-Gemmell <54954662+Patrick-Gemmell@users.noreply.github.com>
2020-01-16 12:47:28 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Space Aliens - CircuitPython Game'
-copyright = u'2020, Mr. Coxall'
+project = u'Shooter Shootout - CircuitPython Game'
+copyright = u'2020, Patrick and Paul'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
